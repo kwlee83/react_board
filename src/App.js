dd03830d9a1f93eb6222fc20361f9bfc341e1ad6
kwlee83 <soulkey7@gmail.com>
2024-02-20 17:47:45 +0900

    fix : input 태그 실습

--- src/App.js
+++ src/App.js
@@ -45,50 +45,82 @@ import React, { useState } from 'react';
 //   );
 // }
 
-const RecordForm = ({numList, setNumList}) => {
+// const RecordForm = ({numList, setNumList}) => {
 
-  const [num, setNum] = useState(0);
+//   const [num, setNum] = useState(0);
 
-  return (
-    <div>
-      <div>현재 숫자 : {num}</div>
-      <button onClick={() => setNum(num+1)}>숫자 증가</button>
-      <button onClick={() => setNum(num-1)}>숫자 감소</button>
-      <button onClick={() => setNum(0)}>숫자 초기화</button>
-      <hr/>
-      <button onClick={() => setNumList([...numList, num])}>숫자 기록</button>
-      <button onClick={() => setNumList([])}>기록 초기화</button>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <div>현재 숫자 : {num}</div>
+//       <button onClick={() => setNum(num+1)}>숫자 증가</button>
+//       <button onClick={() => setNum(num-1)}>숫자 감소</button>
+//       <button onClick={() => setNum(0)}>숫자 초기화</button>
+//       <hr/>
+//       <button onClick={() => setNumList([...numList, num])}>숫자 기록</button>
+//       <button onClick={() => setNumList([])}>기록 초기화</button>
+//     </div>
+//   )
+// }
 
-const RecordList = ({numList}) => {
-  return (
-    <div>
-      <h2>기록된 숫자</h2>
-      {numList.length > 0 ? <div>기록 있음</div> : <div>기록 없음</div>}
+// const RecordList = ({numList}) => {
+//   return (
+//     <div>
+//       <h2>기록된 숫자</h2>
+//       {numList.length > 0 ? <div>기록 있음</div> : <div>기록 없음</div>}
 
-      <ul>
-        {numList.map((num) => (
-            <li>{num}</li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+//       <ul>
+//         {numList.map((num) => (
+//             <li>{num}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+  
+//   const [numList, setNumList] = useState([]);
+
+//   return (
+//     <div style={{margin : "40px auto", 
+//                  width : "800px", 
+//                  textAlign : "center",
+//                 }}>
+//        <RecordForm numList={numList} setNumList={setNumList}/>
+//        <RecordList numList={numList}/>
+//     </div>
+//   )
+// }
 
 const App = () => {
-  
-  const [numList, setNumList] = useState([]);
+
+  const [text, setText] = useState("");
+  const [edit, setEdit] = useState(false);
+
+  let content = <div>
+                  {text}
+                  <button onClick={() => setEdit(true)}>수정</button>
+                </div>
+
+  if(edit){
+    content = 
+    <div>
+        <input type="text" 
+                value={text} 
+                onChange={(e) => {
+                  console.log(e.target.value);
+                  setText(e.target.value);
+                }}
+                />
+        <button onClick={() => setEdit(false)}>수정</button>
+    </div>
+  }
 
   return (
-    <div style={{margin : "40px auto", 
-                 width : "800px", 
-                 textAlign : "center",
-                }}>
-       <RecordForm numList={numList} setNumList={setNumList}/>
-       <RecordList numList={numList}/>
-    </div>
+    <>
+      {content}
+    </>
+    
   )
 }
 
