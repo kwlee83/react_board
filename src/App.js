22c5d00c9e297ae0be415afa8895f20d83388a88
kwlee83 <soulkey7@gmail.com>
2024-02-20 17:27:24 +0900

    fix : react 함수 분리해서 적용

--- src/App.js
+++ src/App.js
@@ -2,47 +2,94 @@ import './App.css';
 
 import React, { useState } from 'react';
 
-function App() {
+// function App() {
 
-  //let name = 'kwleeCoding';
+//   //let name = 'kwleeCoding';
 
-  //let [name, setName] = useState('kwleeCoding');
+//   //let [name, setName] = useState('kwleeCoding');
+
+//   const [num, setNum] = useState(0);
+
+//   const [numList, setNumList] = useState([]);
+
+//   //setTimeout(() => {setNum(num=num+1)}, 1000);
+ 
+//   //<div style={{color : "red"}}>{num}</div>
+
+//   // <header className="App-header">
+       
+//   // <div className="number">{num}</div>
+
+//   // </header>
+
+//   function numRecording() {
+//     setNumList([...numList, num]);
+
+//     setNum(0);
+//   }
+
+//   return (
+//     <div className="App">
+//       <div>현재 숫자 : {num}</div>
+//       <button onClick={() => {setNum(num+1)}}>숫자증가</button>
+//       <button onClick={() => {setNum(num-1)}}>숫자감소</button>
+
+//       <button onClick={numRecording}>숫자기록</button>
+//       <h1>저장된 숫자</h1>
+//       <ul>
+//        {numList.map((num) => (
+//           <li>{num}</li>
+//        ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+const RecordForm = ({numList, setNumList}) => {
 
   const [num, setNum] = useState(0);
 
-  const [numList, setNumList] = useState([]);
-
-  //setTimeout(() => {setNum(num=num+1)}, 1000);
- 
-  //<div style={{color : "red"}}>{num}</div>
-
-  // <header className="App-header">
-       
-  // <div className="number">{num}</div>
-
-  // </header>
-
-  function numRecording() {
-    setNumList([...numList, num]);
-
-    setNum(0);
-  }
-
   return (
-    <div className="App">
+    <div>
       <div>현재 숫자 : {num}</div>
-      <button onClick={() => {setNum(num+1)}}>숫자증가</button>
-      <button onClick={() => {setNum(num-1)}}>숫자감소</button>
+      <button onClick={() => setNum(num+1)}>숫자 증가</button>
+      <button onClick={() => setNum(num-1)}>숫자 감소</button>
+      <button onClick={() => setNum(0)}>숫자 초기화</button>
+      <hr/>
+      <button onClick={() => setNumList([...numList, num])}>숫자 기록</button>
+      <button onClick={() => setNumList([])}>기록 초기화</button>
+    </div>
+  )
+}
 
-      <button onClick={numRecording}>숫자기록</button>
-      <h1>저장된 숫자</h1>
+const RecordList = ({numList}) => {
+  return (
+    <div>
+      <h2>기록된 숫자</h2>
+      {numList.length > 0 ? <div>기록 있음</div> : <div>기록 없음</div>}
+
       <ul>
-       {numList.map((num) => (
-          <li>{num}</li>
-       ))}
+        {numList.map((num) => (
+            <li>{num}</li>
+        ))}
       </ul>
     </div>
-  );
+  )
+}
+
+const App = () => {
+  
+  const [numList, setNumList] = useState([]);
+
+  return (
+    <div style={{margin : "40px auto", 
+                 width : "800px", 
+                 textAlign : "center",
+                }}>
+       <RecordForm numList={numList} setNumList={setNumList}/>
+       <RecordList numList={numList}/>
+    </div>
+  )
 }
 
 export default App;
